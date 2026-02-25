8e21ddda2c9506fc7c1a0709d71b055cf8e8f887
Vladislav Elizarov <velizarov@phoenixit.ru>
2026-02-25 04:48:20 +0300

    iter25 v1

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/tklauser/go-sysconf v0.3.15 // indirect
 	github.com/tklauser/numcpus v0.10.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
--- internal/config/agent.go
+++ internal/config/agent.go
@@ -1,9 +1,13 @@
 package config
 
 import (
-	"flag"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/caarlos0/env/v6"
+	flag "github.com/spf13/pflag"
 )
 
 type AgentConfig struct {
@@ -13,6 +17,16 @@ type AgentConfig struct {
 	Key            string `env:"KEY"`             // ключ
 	RateLimit      int    `env:"RATE_LIMIT"`      // максимальное количество горутин, одновременно отправляющих данные на сервер
 	CryptoKeyPath  string `env:"CRYPTO_KEY"`      // путь к публичному ключу
+	ConfigFile     string `env:"CONFIG"`
+}
+
+type FileAgentConfig struct {
+	Host           string `json:"address"`         // адрес сервера, на который будут отправляться метрики
+	ReportInterval string `json:"report_interval"` // время между отправками метрик на сервер
+	PollInterval   string `json:"poll_interval"`   // время между сборами значений метрик
+	Key            string `json:"key"`             // ключ
+	RateLimit      int    `json:"rate_limit"`      // максимальное количество горутин, одновременно отправляющих данные на сервер
+	CryptoKeyPath  string `json:"crypto_key"`      // путь к публичному ключу
 }
 
 func GetAgentConfig() (AgentConfig, error) {
@@ -24,12 +38,13 @@ func GetAgentConfig() (AgentConfig, error) {
 		return cfg, err
 	}
 
-	hostAddress := flag.String("a", "localhost:8080", "host address")
-	reportInterval := flag.Int("r", 10, "report interval, seconds")
-	pollInterval := flag.Int("p", 2, "poll interval, seconds")
-	key := flag.String("k", "", "secret key")
-	rateLimit := flag.Int("l", 1, "rate limit")
+	hostAddress := flag.StringP("address", "a", "localhost:8080", "host address")
+	reportInterval := flag.IntP("report-interval", "r", 10, "report interval, seconds")
+	pollInterval := flag.IntP("poll-interval", "p", 2, "poll interval, seconds")
+	key := flag.StringP("key", "k", "", "secret key")
+	rateLimit := flag.IntP("rate-limit", "l", 1, "rate limit")
 	cryptoKeyPath := flag.String("crypto-key", "", "crypto key path")
+	configFile := flag.StringP("config", "c", "", "config file")
 	flag.Parse()
 
 	if cfg.Host == "" {
@@ -56,6 +71,67 @@ func GetAgentConfig() (AgentConfig, error) {
 		cfg.CryptoKeyPath = *cryptoKeyPath
 	}
 
-	return cfg, nil
+	if cfg.ConfigFile == "" {
+		cfg.ConfigFile = *configFile
+	}
 
+	err = cfg.parseConfigFile()
+	if err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func (cfg *AgentConfig) parseConfigFile() error {
+	if cfg.ConfigFile == "" {
+		return nil
+	}
+
+	file, err := os.Open(cfg.ConfigFile)
+	if err != nil {
+		return fmt.Errorf("failed to open config file: %w", err)
+	}
+	defer file.Close()
+
+	var fileConfig FileAgentConfig
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&fileConfig)
+	if err != nil {
+		return fmt.Errorf("failed to decode config file: %w", err)
+	}
+
+	if cfg.Host == "" {
+		cfg.Host = fileConfig.Host
+	}
+
+	if cfg.ReportInterval == 0 {
+		reportIntervalDuration, err := time.ParseDuration(fileConfig.ReportInterval)
+		if err != nil {
+			return fmt.Errorf("failed to parse ReportInterval duration: %w", err)
+		}
+		cfg.ReportInterval = int(reportIntervalDuration.Seconds())
+	}
+
+	if cfg.PollInterval == 0 {
+		pollIntervalDuration, err := time.ParseDuration(fileConfig.PollInterval)
+		if err != nil {
+			return fmt.Errorf("failed to parse PollInterval duration: %w", err)
+		}
+		cfg.PollInterval = int(pollIntervalDuration.Seconds())
+	}
+
+	if cfg.Key == "" {
+		cfg.Key = fileConfig.Key
+	}
+
+	if cfg.RateLimit == 0 {
+		cfg.RateLimit = fileConfig.RateLimit
+	}
+
+	if cfg.CryptoKeyPath == "" {
+		cfg.CryptoKeyPath = fileConfig.CryptoKeyPath
+	}
+
+	return nil
 }
